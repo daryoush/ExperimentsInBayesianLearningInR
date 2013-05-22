af6d36685a211f28a6f046871e7163e19716e152
daryoush <dmehrtash@gmail.com>
2013-05-22 11:41:59 -0700

    inlined the model

--- coin/coinstudy.r
+++ coin/coinstudy.r
@@ -1,12 +1,50 @@
 library ("rjags")
 
+
+reportResults <- function(var, name, monitor) {
+  
+  par( mfrow = c( 2, 2 ) )
+  
+  plot( 1:monitor, var, type = 'l', xlab = 'Iteration Number',
+        ylab = name )
+  
+  # the upper left graph is a time-series plot of the monitored
+  # iterations (to visually check for stationarity)
+  
+  plot( density( var ), xlab = 'p', main = '' )
+  
+  # the upper right graph is a density trace of the marginal posterior
+  # distribution for the quantity being monitored
+  
+  acf( as.ts( var[ 1:monitor ], start = 1, 
+              end = monitor, frequency = 1 ), lag.max = 10, main = '' )
+  
+  pacf( as.ts( var[ 1:monitor ], start = 1, 
+               end = monitor, frequency = 1 ), lag.max = 10, main = '' )
+  
+  
+  return (c( mean(var), sd( var ), quantile(var, 
+                                            probs = c( 0.025, 0.975 ) ) ))
+}
+
 setwd("~/Documents/workspace/ExperimentsInBayesianLearningInR/coin")
+
+modelstring="
+model {
+  p ~ dunif( 0, 1 )
+  for( i in 1 : n)  {
+    h[i] ~ dbern( p )
+  }
+  hh <-  p * p
+  tt <- (1-p) * (1-p)
+}
+"
 
 modeldata  <- list(h=c(1,1,1,1,1,1,1,1,1,1,0,0,0,0), n=14)
 
 flips.inits.1 <- list(p=0.5)
 
-flips.run.1 <- jags.model( file = "coinmodel.txt", data = modeldata, 
+flips.run.1 <- jags.model( textConnection(modelstring), data = modeldata, 
                             inits = flips.inits.1 )
 
 set.seed( 65748392 )
@@ -15,63 +53,26 @@ n.burnin <- 1000
 
 update( flips.run.1, n.iter = n.burnin )
 
-n.monitor <- 100000
+n.monitor <- 10000
 
 flips.run.1.results <- jags.samples( flips.run.1, 
-                                      variable.names = c( "p" , "hh"), 
+                                      variable.names = c( "p" , "hh", "tt"), 
                                       n.iter = n.monitor )
 
 p.star <- flips.run.1.results$p
 hh.star <- flips.run.1.results$hh
-
-par( mfrow = c( 2, 2 ) )
-
-plot( 1:n.monitor, p.star, type = 'l', xlab = 'Iteration Number',
-      ylab = 'p' )
-
-# the upper left graph is a time-series plot of the monitored
-# iterations (to visually check for stationarity)
-
-plot( density( p.star ), xlab = 'p', main = '' )
-
-# the upper right graph is a density trace of the marginal posterior
-# distribution for the quantity being monitored
-
-acf( as.ts( p.star[ 1:n.monitor ], start = 1, 
-            end = n.monitor, frequency = 1 ), lag.max = 10, main = '' )
-
-pacf( as.ts( p.star[ 1:n.monitor ], start = 1, 
-             end = n.monitor, frequency = 1 ), lag.max = 10, main = '' )
+tt.star <- flips.run.1.results$tt
 
 
-c( mean( p.star ), sd( p.star ), quantile(p.star, 
-                                             probs = c( 0.025, 0.975 ) ) )
-
-
-
+### probability of head
+reportResults(p.star, 'p', n.monitor)
 
 #####  Probability of two heads
-
-par( mfrow = c( 2, 2 ) )
-
-plot( 1:n.monitor, hh.star, type = 'l', xlab = 'Iteration Number',
-      ylab = 'hh' )
-
-# the upper left graph is a time-series plot of the monitored
-# iterations (to visually check for stationarity)
-
-plot( density( hh.star ), xlab = 'p', main = '' )
-
-# the upper right graph is a density trace of the marginal posterior
-# distribution for the quantity being monitored
-
-acf( as.ts( hh.star[ 1:n.monitor ], start = 1, 
-            end = n.monitor, frequency = 1 ), lag.max = 10, main = '' )
-
-pacf( as.ts( hh.star[ 1:n.monitor ], start = 1, 
-             end = n.monitor, frequency = 1 ), lag.max = 10, main = '' )
+reportResults(hh.star, 'hh', n.monitor)
 
 
-c( mean( hh.star ), sd( hh.star ), quantile(hh.star, 
-                                          probs = c( 0.025, 0.975 ) ) )
+#####  Probability of two tails
+reportResults(tt.star, 'tt', n.monitor)
+
+
 
